f6783e5e1cde7f2e3053ddd8d3ee8fdcb9bd71cb
sunny setia <sunnysetia93@gmail.com>
2019-05-22 15:43:15 +0530

    travis CI

--- tests/header.test.js
+++ tests/header.test.js
@@ -10,7 +10,7 @@ beforeEach(async ()=>{
     // })
     // page = await browser.newPage();
     page = await Page.build();
-    await page.goto('localhost:3000');
+    await page.goto('http://localhost:3000');
 })
 
 afterEach( async ()=>{
