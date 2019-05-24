c99205a4fbc8bf140ceb0eee2203d1030edbc699
sunny setia <sunnysetia93@gmail.com>
2019-05-24 11:52:14 +0530

    s3 bucket first commit

--- config/dev.js
+++ config/dev.js
@@ -4,7 +4,9 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://localhost:27017/blog_dev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIA4WRM7BW5RGWUQMKK',
+  secretAccessKey: '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'
 };
 
   // mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
---
+++ routes/uploadRoutes.js
@@ -0,0 +1,13 @@
+const AWS = require('aws-sdk');
+const keys = require('../config/keys');
+
+const s3 = new AWS.S3({
+    accessKeyId:keys.accessKeyId,
+    secretAccessKey:keys.secretAccessKey
+})
+
+module.exports = app => {
+    app.get('/api/upload', (req,res)=>{
+
+    })
+};
