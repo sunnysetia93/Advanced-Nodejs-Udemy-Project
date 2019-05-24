3e79519c2ee9d41fd4054dc28594a745960c8aa1
sunny setia <sunnysetia93@gmail.com>
2019-05-24 12:23:48 +0530

    dev

--- config/dev.js
+++ config/dev.js
@@ -4,7 +4,10 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://localhost:27017/blog_dev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIA4WRM7BW5YUME4OZE',
+  secretAccessKey: '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'
+
 };
 
   // mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
